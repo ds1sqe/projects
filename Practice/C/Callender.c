efd8b1d629dfa4722e52cd8f0cf49bf728f1068f
ds1sqe <ds1sqe@mensakorea.org>
2021-06-20 15:05:55 +0900

    102

---
+++ Practice/C/Callender.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define LINE1() printf("=======================================\n")
+#define LINE2() printf("---------------------------------------\n")
+
+time_t t = time(NULL);
+struct tm now =*localtime(&t);
+
+
+void whatIsdow(int day){
+//INPUT DAY
+//OUTPUT DOW
+
+char dow;
+return dow;
+}
+	
+void main() {
+    printf("tm : %d-%d-%d %d:%d:%d\n",
+    tm.now_year+1900,tm.now_mon+1,tm.now_year)
+}
